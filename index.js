9e917c0c603eed8855841d2cd692aeb58d8ffb1a
mkokor <matijakokorr@gmail.com>
2023-03-08 20:07:19 +0100

    Implementirana validacija podataka prilikom registracije

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const bazaPodataka = require("./bazaPodataka/inicijalizacijaBazePodataka.js");
+const sigurnost = require("./pomoćniModuli/sigurnostPodataka.js");
 
 
 const application = express();
@@ -11,12 +12,41 @@ application.use(express.static("public"));
 application.use(bodyParser.json());
 
 
+// POMOĆNE FUNKCIJE
+
+// Funkcija provjerava da li u bazi podataka postoji zapis koji u koloni pod nazivom 
+// koji je sadržan u parametru "nazivAtributa", posjeduje vrijednost koja je sadržana 
+// u parametru "vrijednostAtributa".
+const postojiLiKorisničkiRačun = (nazivAtributa, vrijednostAtributa) => {
+  return new Promise((resolve, reject) => {
+    bazaPodataka.KorisničkiRačun.findOne({ "where": { [nazivAtributa]: vrijednostAtributa } })
+      .then((korisničkiRačun) => {
+        resolve(korisničkiRačun ? true : false);
+      })
+      .catch(() => {
+        reject("Greška u pristupu bazi podataka!");
+      });
+  });
+}
+
+const validirajEmail = (email) => {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+}
+
+
 // RUTE
 
 // URL: http://localhost:3000/registracija
 // TIJELO ZAHTJEVA: { email: *, korisničkoIme: *, lozinka: * }
 // ODGOVOR: { email: true/false, korisničkoIme: true/false, lozinka: true/false }
 application.post("/registracija", (request, response) => {
+  let validacija = [];
+  validacija.push(postojiLiKorisničkiRačun("email", request.body.email));
+  validacija.push(postojiLiKorisničkiRačun("korisničkoIme", request.body.korisničkoIme));
+  Promise.all(validacija)
+    .then((rezultat) => {
+      console.log(rezultat);
+    });
   response.setHeader("Content-Type", "application/json");
   response.status(200);
   response.send(JSON.stringify({
---
+++ pomoćniModuli/sigurnostPodataka.js
@@ -0,0 +1,43 @@
+// Ovaj modul daje dvije funkcije, enkriptujPodatak i dekriptujPodatak. 
+// Namjena funkcija je jasna, a koristit će se za zaštitu korisničkih
+// lozinki.
+
+
+const bcrypt = require("bcrypt");
+
+
+// Parametar "podatak" je tekst (tj. string) koji se enkodira.
+const enkriptujPodatak = (podatak) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(podatak, Math.round(Math.random() * 10))
+      .then((enkriptovanPodatak) => {
+        resolve(enkriptovanPodatak);
+      })
+      .catch(() => {
+        reject("Greška prilikom heširanja!");
+      });
+  });
+}
+
+// Ova funkcija prima podatak ("plain text") i neki kod, te provjerava da li je taj kod zapravo
+// enkriptovana verzija tog podatka,
+const dekriptujPodatak = (podatak, enkriptovanPodatak) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(podatak, enkriptovanPodatak)
+      .then((podudaranje) => {
+        resolve(podudaranje);
+      })
+      .catch(() => {
+        reject("Greška prilikom dekripcije!");
+      });
+  });
+}
+
+
+module.exports = {
+  enkriptujPodatak: enkriptujPodatak,
+  dekriptujPodatak: dekriptujPodatak
+}
+
+
+
